772bda98b2f4ddf616f64bb943489ffc1cf46329
rajkharvar <rajkharvar1808@gmail.com>
2020-01-19 12:13:27 +0530

    another commit

--- contracts/Identity.sol
+++ contracts/Identity.sol
@@ -1,32 +1,55 @@
 pragma solidity ^0.5.0;
 
 contract Identity {
-    struct Member {
-        string fname;
-        string lname;
-        uint age;
+    struct HealthRec {
+		bool exists;
         string date;
+        string desc;
+        string imghash;
+    }
+    struct Person {
+		bool exists;
+        string fullname;
+        string dob;
+        string addr;
+        string gender;
+        string bloodgroup;
+        uint mobile;
+        string imghash;
+        HealthRec[] healthdata;
     }
     bytes32 public uniqueKey;
-    // * registered index
-    mapping(bytes32 => Member) public members;
-    mapping(bytes32 => bool) public subscribed;
-    // hash the user details and return bytes32 (UNIQUE STRING)
-    function hashIt (string memory fname, uint age, string memory lname) public{
-      uniqueKey = keccak256(abi.encode(fname, age, lname));
+    mapping(bytes32 => Person) public members;
+    function hashIt (string memory name, string memory dob, string memory gender) private{
+        uniqueKey = keccak256(abi.encode(name, dob, gender));
     }
-    // add the details of member to ethereum blockchain
-    function addMember(string memory _fname, string memory _lname, uint _age, string memory _date) public returns (bytes32) {
-        hashIt(_fname, _age, _lname);
-        require(!subscribed[uniqueKey], "Already registered");
-        members[uniqueKey].fname = _fname;
-        members[uniqueKey].lname = _lname;
-        members[uniqueKey].age = _age;
-        members[uniqueKey].date = _date;
-        subscribed[uniqueKey] = true;
+    function addHealthData(bytes32 _uniqueKey, string memory _date, string memory _desc, string memory _imghash) public{
+        require(members[_uniqueKey].exists, "No User Found");
+        members[_uniqueKey].healthdata.push(HealthRec({exists: true, date: _date, desc: _desc, imghash: _imghash}));
     }
-    // get the member details
-    function getMember (bytes32 _candidateId) public view returns (string memory, string memory, uint, string memory) {
-        return (members[_candidateId].fname, members[_candidateId].lname, members[_candidateId].age, members[_candidateId].date);
+    function fetchHealthData(bytes32 _uniqueKey, uint index) public view returns (string memory) {
+        require(members[_uniqueKey].exists, "No User Found");
+        require(members[_uniqueKey].healthdata[index].exists, "No Health Record Found");
+        return (members[_uniqueKey].healthdata[index].desc);
+    }
+    function addPersonDetails(string memory _name, string memory _dob, string memory _addr, string memory _gender,
+        string memory _bloodgroup, uint _mobile, string memory _imghash) public returns (bytes32) {
+        hashIt(_name, _dob, _gender);
+        require(!members[uniqueKey].exists, "Already registered");
+        members[uniqueKey].exists = true;
+        members[uniqueKey].fullname = _name;
+        members[uniqueKey].dob = _dob;
+        members[uniqueKey].addr = _addr;
+        members[uniqueKey].gender = _gender;
+        members[uniqueKey].bloodgroup = _bloodgroup;
+        members[uniqueKey].mobile = _mobile;
+        members[uniqueKey].imghash = _imghash;
+    }
+    function getPersonDetails(bytes32 _candidateId) public view returns (string memory, string memory, string memory,
+    string memory, string memory, uint, string memory) {
+        require(members[_candidateId].exists, "No User Found");
+        return (members[_candidateId].fullname, members[_candidateId].dob,
+        members[_candidateId].addr, members[_candidateId].gender, members[_candidateId].bloodgroup,
+        members[_candidateId].mobile, members[_candidateId].imghash);
     }
 }
