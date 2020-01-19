2de44df327e1f89c82124621b53c834413287d2e
rajkharvar <rajkharvar1808@gmail.com>
2020-01-19 06:30:41 +0530

    lazy commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,30 @@
-import React, { Component } from 'react'
-import Identity from './contracts/Identity.json'
-import getWeb3 from './getWeb3'
-
-import './App.css'
-
+import React, { Component } from "react"
+import Identity from "./contracts/Identity.json"
+import getWeb3 from "./getWeb3"
+import { Form, Button } from "react-bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./App.css"
 class App extends Component {
   state = {
     storageValue: 0,
     web3: null,
     accounts: null,
     contract: null,
-    fname: '',
-    lname: '',
-    age: 0,
-    date: ''
+    fname: "",
+    lname: "",
+    dob: "",
+    addr: "",
+    gender: "",
+    bloodgroup: "",
+    mobile: 0,
+    unkey: null
   }
-
   componentDidMount = async () => {
     try {
       // Get network provider and web3 instance.
       const web3 = await getWeb3()
-
       // Use web3 to get the user's accounts.
       const accounts = await web3.eth.getAccounts()
-
       // Get the contract instance.
       const networkId = await web3.eth.net.getId()
       const deployedNetwork = Identity.networks[networkId]
@@ -31,10 +32,9 @@ class App extends Component {
         Identity.abi,
         deployedNetwork && deployedNetwork.address
       )
-
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance })
+      this.setState({ web3, accounts, contract: instance }, )
       console.log(accounts)
     } catch (error) {
       // Catch any errors for any of the above operations.
@@ -44,69 +44,118 @@ class App extends Component {
       console.error(error)
     }
   }
-
   addMember = async e => {
     e.preventDefault()
     const { accounts, contract } = this.state
-
+    console.log(this.state)
+    // const test = await contract.methods
     const testData = await contract.methods
-      .addMember(
+      .addPersonDetails(
         this.state.fname,
-        this.state.lname,
-        this.state.age,
-        this.state.date
+        this.state.dob,
+        this.state.addr,
+        this.state.gender,
+        this.state.bloodgroup,
+        Number(this.state.mobile),
+        "some random string"
       )
       .send({ from: accounts[0] })
-      if (testData) {
-        this.setState({ fname: '', lname: '', age: 0, date: '' })
-      }
+      let testit
+    contract.methods.uniqueKey().call({ from: accounts[0] }).then(function(result) {
+      testit = result
+    }).then(() => {
+      this.setState({ unkey: testit })
+    })
+    if (testData) {
+      console.log(testData)
+      this.setState({ fname: "", lname: "", mobile: 0, dob: "" })
+    }
   }
-
   onChange = e => {
     this.setState({ [e.target.name]: e.target.value })
   }
-
-  render () {
+  render() {
     if (!this.state.web3) {
       return <div>Loading Web3, accounts, and contract...</div>
     }
     return (
-      <div className='App'>
+      <div className="App">
         <h1>{this.state.accounts[0]}</h1>
-        <form onSubmit={this.addMember}>
-          <input
-            type='text'
-            value={this.state.fname}
-            placeholder='First name'
-            onChange={this.onChange}
-            name='fname'
-          />
-          <input
-            type='text'
-            value={this.state.lname}
-            placeholder='Last name'
-            onChange={this.onChange}
-            name='lname'
-          />
-          <input
-            type='number'
-            value={this.state.age}
-            placeholder='Age'
-            onChange={this.onChange}
-            name='age'
-          />
-          <input
-            type='text'
-            value={this.state.date}
-            placeholder='date'
-            onChange={this.onChange}
-            name='date'
-          />
-          <button type='submit'>Add Member</button>
-        </form>
+        {
+          this.state.unkey ? (
+          <h2>{this.state.unkey}</h2>
+          ): ""
+        }
+        <Form onSubmit={this.addMember}>
+          <Form.Group>
+          <Form.Label>First Name</Form.Label>
+            <Form.Control
+              type="text"
+              value={this.state.fname}
+              placeholder="First name"
+              onChange={this.onChange}
+              name="fname"
+              required
+            />
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control
+              type="text"
+              value={this.state.lname}
+              placeholder="Last name"
+              onChange={this.onChange}
+              name="lname"
+              required
+            />
+            <Form.Label>Gender</Form.Label>
+            <Form.Control name="gender" as="select" onChange={this.onChange} value={this.state.gender}>
+              <option name="gender" value="male">Male</option>
+              <option name="gender" value="female">Female</option>
+              <option name="gender" value="other">Other</option>
+            </Form.Control>
+            <Form.Label>Date of Birth</Form.Label>
+            <Form.Control
+              type="date"
+              value={this.state.dob}
+              placeholder="Date of Birth"
+              onChange={this.onChange}
+              name="dob"
+              required
+            />
+            <Form.Label>Blood Group</Form.Label>
+            <Form.Control
+              type="text"
+              value={this.state.bloodgroup}
+              placeholder="Blood Group"
+              onChange={this.onChange}
+              name="bloodgroup"
+              required
+            />
+            <Form.Label>Address</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows="3"
+              value={this.state.addr}
+              placeholder="Address"
+              onChange={this.onChange}
+              name="addr"
+              required
+            />
+            <Form.Label>Mobile Number</Form.Label>
+            <Form.Control
+              type="number"
+              value={this.state.mobile}
+              placeholder="Blood Group"
+              onChange={this.onChange}
+              name="mobile"
+              required
+            />
+            <Button type="submit" variant="primary">
+              Add Member
+            </Button>
+          </Form.Group>
+        </Form>
       </div>
     )
   }
 }
-
 export default App
